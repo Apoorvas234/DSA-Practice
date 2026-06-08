a01846a1e8981e12fd7244e115fa035ea4f3a743
Apoorva <apoorvas562@gmail.com>
2026-06-08 23:47:02 +0530

    Search in Descending sorted Array

---
+++ Searching/Descending.java
@@ -0,0 +1,26 @@
+public class Descending{
+    public int search(int arr[],int target){
+        int n = arr.length;
+        int low = 0 ,high = n-1;
+        while(low<=high){
+            int mid = low+(high-low)/2;
+            if(arr[mid]==target) return mid;
+            else if(arr[mid]<target){
+                 high = mid-1;
+              
+            }
+            else if(arr[mid]>target){
+               low = mid+1;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args){
+        Descending obj = new  Descending();
+
+        int[] arr = {98,89,87,67,56,45,43,23,11};
+        int ans = obj.search(arr,67);
+
+        System.out.println(ans);
+    }
+}
